7c77bbd3b5510fb302aa90eb96da47136deadf38
rayaadhhoda <138174762+rayaadhhoda@users.noreply.github.com>
2026-05-09 22:04:16 -0700

    Add files via upload

---
+++ hello.asm
@@ -0,0 +1,45 @@
+.global
+
+.text
+LDI %b, #12288
+
+LDI %a, #72
+STR %a, %b [0]
+
+LDI %a, #101
+STR %a, %b [0]
+
+LDI %a, #108
+STR %a, %b [0]
+
+LDI %a, #108
+STR %a, %b [0]
+
+LDI %a, #111
+STR %a, %b [0]
+
+LDI %a, #44
+STR %a, %b [0]
+
+LDI %a, #32
+STR %a, %b [0]
+
+LDI %a, #87
+STR %a, %b [0]
+
+LDI %a, #111
+STR %a, %b [0]
+
+LDI %a, #114
+STR %a, %b [0]
+
+LDI %a, #108
+STR %a, %b [0]
+
+LDI %a, #100
+STR %a, %b [0]
+
+LDI %a, #33
+STR %a, %b [0]
+
+EXIT
